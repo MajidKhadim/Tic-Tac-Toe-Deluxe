127056e8d8524319662467b491e18bface2dd9e9
Majid Khadim <110695542+MajidKhadim@users.noreply.github.com>
2024-03-10 23:15:21 +0500

    Update app.js
    solved a bug

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ resetBtn.addEventListener('click',()=>{
 })
 function checkWin() {
     let winConditions = [
-        [0, 2, 4], [3, 4, 5], [6, 7, 8],
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
         [0, 3, 6], [1, 4, 7], [2, 5, 8],
         [0, 4, 8], [2, 4, 6]
     ];
